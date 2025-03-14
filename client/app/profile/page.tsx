3e9a505b752f2a7cee6d930eb4165e1bda98b0ce
PuuPSEK <zai.nikita@mail.ru>
2025-03-14 18:56:41 +0700

    profile page

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -7,7 +7,9 @@ export default function ProfilePage() {
   return (
     <section>
       <Header />
-      <h1>Profile Page Content</h1>
+      <div className="pt-[150px]">
+        <h1>Profile Page Content</h1>
+      </div>
     </section>
   );
 }
